53408872118bbb25007a5f8336e0d2b6836566e0
Mike Leonard <mike.r.leonard@gmail.com>
2015-06-01 01:12:25 +1000

    Modifications to make node/vagrant play nicely on windows. Namely moving node_modules to a directory outside of the vagrant share and symlinking to it.

--- shell/provision.sh
+++ shell/provision.sh
@@ -17,12 +17,21 @@ npm install -g bower
 apt-get install -y g++
 (cd /usr/local/lib/ && git clone https://github.com/sass/sassc.git --branch 3.2.1 --depth 1)
 (cd /usr/local/lib/ && git clone https://github.com/sass/libsass.git --branch 3.2.1 --depth 1)
-(cd /usr/local/lib/sassc/ && SASS_LIBSASS_PATH="/usr/local/lib/libsass"; make)
+export SASS_LIBSASS_PATH="/usr/local/lib/libsass"
+(cd /usr/local/lib/sassc/ && make)
 (cd /usr/local/bin/ && ln -s ../lib/sassc/bin/sassc sassc)
+
+# There are issues have the node_modules in the shared /vagrant folder under windows
+# as the file paths can get longer than windows allows (260 characters). Therefore we
+# create a directory outside of the share and create a symlink to it.
+mkdir /node_modules
+chown vagrant:vagrant /node_modules
+(cd /vagrant && ln -s /node_modules/ node_modules)
 
 # Install project dependencies
 (cd /vagrant && npm install --ignore-scripts)
-(cd /vagrant && bower install --config.interactive=false)
+(cd /vagrant && bower install --config.interactive=false --allow-root)
+(cd /vagrant && npm rebuild node-sass)
 
 # Initialize submodules
 (cd /vagrant && git submodule update --init --recursive)

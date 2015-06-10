8fff27df0475db25fdbb8435d5b171deffb9cfa6
David Thompson <davethompson21@gmail.com>
2015-06-10 16:26:43 +0100

    Minor cosmetic change

--- assets/javascripts/dashboard.js
+++ assets/javascripts/dashboard.js
@@ -1,3 +1,3 @@
 $(document).ready(function() {
-  $(".ae-requests").animate({ backgroundColor: "red", borderRadius: "10px" }, 1500 ).animate({ backgroundColor: "white" }, 1500 );
+  $(".ae-requests").animate({ backgroundColor: "red", borderRadius: "5px" }, 1500 ).animate({ backgroundColor: "white" }, 1500 );
 });
